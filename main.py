cb8ab1ac7b3624acb75ceb0731befdc12f9d1175
bodasingip <158056845+bodasingip@users.noreply.github.com>
2024-02-05 16:40:14 +0530

    Added files

---
+++ main.py
@@ -0,0 +1,43 @@
+
+import ldclient
+from ldclient import Context
+from ldclient.config import Config
+
+# Set sdk_key to your LaunchDarkly SDK key before running
+sdk_key = "sdk-78ede243-f829-4c0a-8ed1-2c0e92892246"
+
+# Set feature_flag_key to the feature flag key you want to evaluate
+feature_flag_key = "textReturn"
+
+def show_message(s):
+  print("*** %s" % s)
+  print()
+
+if __name__ == "__main__":
+  if not sdk_key:
+    show_message("Please edit main.py to set sdk_key to your LaunchDarkly SDK key first")
+    exit()
+
+  ldclient.set_config(Config(sdk_key))
+
+  # The SDK starts up the first time ldclient.get() is called
+  if ldclient.get().is_initialized():
+    show_message("SDK successfully initialized!")
+  else:
+    show_message("SDK failed to initialize")
+    exit()
+
+  # Set up the evaluation context. This context should appear on your LaunchDarkly contexts
+  # dashboard soon after you run the demo.
+  context = Context.builder('test-user-key').name('Praveen').build()
+
+  flag_value = ldclient.get().variation(feature_flag_key, context, False)
+
+  show_message("Feature flag '%s' is %s for this context" % (feature_flag_key, flag_value))
+
+  # Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
+  # events to LaunchDarkly before the program exits. If analytics events are not delivered,
+  # the context properties and flag usage statistics will not appear on your dashboard. In a
+  # normal long-running application, the SDK would continue running and events would be
+  # delivered automatically in the background.
+  ldclient.get().close()
